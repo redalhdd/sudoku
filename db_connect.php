f622374157c7a92cc6dd767131782b67ce96240b
redalhdd <redalahdoudi@gmail.com>
2024-12-11 17:56:42 +0100

    version commune v2

--- db_connect.php
+++ db_connect.php
@@ -3,7 +3,8 @@ session_start();
 // Change this to your connection info.
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+//$DATABASE_PASS = '';
+$DATABASE_PASS = 'root';
 $DATABASE_NAME = 'sudoku';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
